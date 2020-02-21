b28f95e1f8817744f702bde8466ed5cc7fa52833
viveksarangi <viveksarangi@gmail.com>
2020-02-21 17:47:30 -0600

    python 2.7 to 3 conversion

--- First_process_vcf.py
+++ First_process_vcf.py
@@ -15,18 +15,18 @@ import time
 def argument_parse():
     """Parses the command line arguments"""
     parser = argparse.ArgumentParser(description='Preprocessing of vcf file')
-    parser.add_argument("-V", "--VCF_file", help="Path to VCF file", required=True, type=Util.FileValidator)
-    parser.add_argument("-O", "--Output_dir", help="Path to directory where results will be written", required=True)
-    parser.add_argument("-S", "--Sample_name", help="Name of the sample", required=True)
+    parser.add_argument("-v", "--VCF_file", help="Path to VCF file", required=True, type=Util.FileValidator)
+    parser.add_argument("-o", "--Output_dir", help="Path to directory where results will be written", required=True)
+    parser.add_argument("-s", "--Sample_name", help="Name of the sample", required=True)
     parser.add_argument("-l", "--chromosome_number", help="chromosome number ")
     parser.add_argument("-f", "--fast_option", help="run all chromosome in parallel.Uses more memory ",
                         action='store_true')
     return parser
 
 
-def shapeit(vcf_file, output_dir, sample_name, chromosome_number):
+def run_shapeit(vcf_file, output_dir, sample_name, chromosome_number):
     """running shapeit on vcf file"""
-    map = os.path.join(config["SHAPIT_REF"], "genetic_map_chr" + chromosome_number + "_combined_b37.txt")
+    input_map = os.path.join(config["SHAPIT_REF"], "genetic_map_chr" + chromosome_number + "_combined_b37.txt")
     hap = os.path.join(config["SHAPIT_REF"], "1000GP_Phase3_chr" + chromosome_number + ".hap.gz")
     leg = os.path.join(config["SHAPIT_REF"], "1000GP_Phase3_chr" + chromosome_number + ".minimal.legend.gz")
     sam = os.path.join(config["SHAPIT_REF"], "1000GP_Phase3.sample")
@@ -38,7 +38,7 @@ def shapeit(vcf_file, output_dir, sample_name, chromosome_number):
     vcf = vcf_file
     cmd = ' '.join([shapeit, "check",
                     "--input-vcf", vcf,
-                    "--input-map", map,
+                    "--input-map", input_map,
                     "--input-ref", hap, leg, sam,
                     "--output-log", chk,
                     "--output-max", out])
@@ -47,7 +47,7 @@ def shapeit(vcf_file, output_dir, sample_name, chromosome_number):
     if os.path.exists(chk + ".snp.strand.exclude") and os.path.getsize(chk + ".snp.strand.exclude") > 0:
         cmd = ' '.join([shapeit, "--thread 4",
                         "--input-vcf", vcf,
-                        "--input-map", map,
+                        "--input-map", input_map,
                         "--exclude-snp", chk + ".snp.strand.exclude",
                         "--input-ref", hap, leg, sam,
                         "--output-log", out,
@@ -55,7 +55,7 @@ def shapeit(vcf_file, output_dir, sample_name, chromosome_number):
     else:
         cmd = ' '.join([shapeit, "--thread 4",
                         "--input-vcf", vcf,
-                        "--input-map", map,
+                        "--input-map", input_map,
                         "--input-ref", hap, leg, sam,
                         "--output-log", out,
                         "--output-max", out])
@@ -71,19 +71,19 @@ def shapeit(vcf_file, output_dir, sample_name, chromosome_number):
 
 def read_1000_genome(vcf, chromosome_number=None):
     """reads the 1000 genome file and returns a dict of positions"""
-    if chromosome_number == None:
+    if chromosome_number is None:
         print("---------- reading all SNPs from 1000 genome file----------")
     else:
         print("---------- reading chr" + chromosome_number + " SNPs from 1000 genome file----------")
     g1k_snp = config["G1K_SNP"]
     if g1k_snp.endswith(".gz"):
-        g1k_snp_fh = gzip.open(g1k_snp,'rt')
+        g1k_snp_fh = gzip.open(g1k_snp, 'rt')
     else:
         g1k_snp_fh = open(g1k_snp)
     g1k_chr_status = Util.check_vcf_with_chr_or_not(g1k_snp)
     vcf_chr_status = Util.check_vcf_with_chr_or_not(vcf)
     chromosome_number = chromosome_number
-    if g1k_chr_status != "with_chr" and vcf_chr_status == "with_chr" and chromosome_number != None:
+    if g1k_chr_status is not "with_chr" and vcf_chr_status is "with_chr" and chromosome_number is not None:
         g1k_chromosome_number = "chr" + chromosome_number
     else:
         g1k_chromosome_number = chromosome_number
@@ -94,7 +94,7 @@ def read_1000_genome(vcf, chromosome_number=None):
             continue
         if vcf_chr_status != g1k_chr_status and g1k_chr_status == "without_chr":
             line[0] = "chr" + line[0]
-        if chromosome_number != None and line[0] != g1k_chromosome_number:
+        if chromosome_number is not None and line[0] is not g1k_chromosome_number:
             continue
         ref = line[2]
         alt = line[3]
@@ -109,19 +109,12 @@ def read_1000_genome(vcf, chromosome_number=None):
 
 def extract_heterozygous_snps(vcf, vcf_out, chromosome_number, g1k_snp_dict):
     """extracts the SNPs present in 1000G and keeps only mono allelic heterozygous snps"""
-    g1k_snp = config["G1K_SNP"]
     vcf_out_fh = open(vcf_out, 'w')
-
-    if g1k_snp.endswith(".gz"):
-        g1k_snp_fh = gzip.open(g1k_snp)
-    else:
-        g1k_snp_fh = open(g1k_snp)
     print("filtering vcf file......")
     if vcf.endswith(".gz"):
         vcf_fh = gzip.open(vcf)
     else:
         vcf_fh = open(vcf)
-
     vcf_chr_status = Util.check_vcf_with_chr_or_not(vcf)
     if vcf_chr_status == "without_chr":
         vcf_chromosome_number = chromosome_number
@@ -137,18 +130,18 @@ def extract_heterozygous_snps(vcf, vcf_out, chromosome_number, g1k_snp_dict):
         if line[0] != vcf_chromosome_number:
             continue
         filter_col = line[6]
-        GT = ""
+        gt = ""
         for n, j in enumerate(line[8].split(":")):
             if j == "GT":
-                GT = line[9].split(":")[n]
+                gt = line[9].split(":")[n]
         pos = "_".join([line[0], line[1], line[3], line[4]])
         if pos in pos_dict:
             continue
         else:
             pos_dict[pos] = 1
-        if pos not in g1k_snp_dict or filter_col != "PASS" or GT != "0/1":
+        if pos not in g1k_snp_dict or filter_col != "PASS" or gt != "0/1":
             continue
-        new_i = "\t".join([line[0], line[1], line[2], line[3], line[4], ".", ".", ".", "GT", GT])
+        new_i = "\t".join([line[0], line[1], line[2], line[3], line[4], ".", ".", ".", "GT", gt])
         vcf_out_fh.write(new_i + "\n")
     vcf_out_fh.close()
 
@@ -161,25 +154,18 @@ def running_preprocessing(vcf, output_dir, sample_name, chromosome_number, g1k_s
     extract_heterozygous_snps(vcf, vcf_snp_het, chromosome_number, g1k_snp_dict)
     print("---------------Done extracting SNPS-----------------------")
     print("2. Generating phased VCF")
-    shapeit(vcf_snp_het, output_dir, sample_name, chromosome_number)
+    run_shapeit(vcf_snp_het, output_dir, sample_name, chromosome_number)
     print("---------------Done phasing vcf---------------------------")
 
 
 def main():
     script_path = os.path.dirname(os.path.abspath(sys.argv[0]))
     parser = argument_parse()
-    try:
-        arg = parser.parse_args()
-    except:
-        if "JOB_ID" not in os.environ:
-            print(sys.exc_info()[1])
-            exit()
-        else:
-            print(sys.exc_info()[1])
-            exit()
-    global config
+    arg = parser.parse_args()
+
     config_file = os.path.join(script_path, "config_file/config.txt")
     try:
+        global config
         config = Util.ParseConfig(config_file)
     except:
         if "JOB_ID" not in os.environ:
